de36f99951622705e4f50a8f9d1cdb13efae685b
Tobi Adesanya <tobi.adesanya@babbangona.com>
2019-10-01 17:23:30 +0100

    WIP - Recycler Search

--- app/src/main/java/com/babbangona/barcodescannerproject/MsaAdapter.java
+++ app/src/main/java/com/babbangona/barcodescannerproject/MsaAdapter.java
@@ -1,8 +1,12 @@
 package com.babbangona.barcodescannerproject;
 
+import android.content.Context;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,10 +14,13 @@ import com.babbangona.barcodescannerproject.model.msaT;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MsaAdapter extends RecyclerView.Adapter<MsaAdapter.MyViewHolder> {
+public class MsaAdapter extends RecyclerView.Adapter<MsaAdapter.MyViewHolder> implements Filterable {
         private List<msaT> msaList;
+        private List<msaT> msaListFiltered;
+       // private Context context;
 
         public class MyViewHolder extends RecyclerView.ViewHolder{
             public TextView msaStaff, msaName;
@@ -27,6 +34,8 @@ public class MsaAdapter extends RecyclerView.Adapter<MsaAdapter.MyViewHolder> {
 
         public MsaAdapter (List<msaT> msaList){
             this.msaList = msaList;
+            this.msaListFiltered = msaList;
+
         }
 
         @Override
@@ -38,7 +47,7 @@ public class MsaAdapter extends RecyclerView.Adapter<MsaAdapter.MyViewHolder> {
 
         @Override
         public void onBindViewHolder(MyViewHolder holder, int position){
-            msaT msaT = msaList.get(position);
+            msaT msaT = msaListFiltered.get(position);
             holder.msaStaff.setText(msaT.getStaff_id());
             holder.msaName.setText(msaT.getFullname());
         }
@@ -46,6 +55,37 @@ public class MsaAdapter extends RecyclerView.Adapter<MsaAdapter.MyViewHolder> {
         @Override
         public int getItemCount(){
             return msaList.size();
+        }
+
+        @Override
+        public Filter getFilter(){
+            return new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence charSequence) {
+                    String charString = charSequence.toString();
+                    if (charString.isEmpty()) {
+                        msaListFiltered = msaList;
+                    } else{
+                        List<msaT> filteredList = new ArrayList<>();
+                        for (msaT row : msaList){
+                            //checking for name or staffID match
+                            if(row.getStaff_id().toLowerCase().contains(charString.toLowerCase()) || row.getFullname().contains((charString.toLowerCase()))){
+                                filteredList.add(row);
+                            }
+                        }
+                        msaListFiltered = filteredList;
+                    }
+                    FilterResults filterResults = new FilterResults();
+                    filterResults.values = msaListFiltered;
+                    return filterResults;
+                }
+
+                @Override
+                protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                    msaListFiltered = (ArrayList<msaT>) filterResults.values;
+                    notifyDataSetChanged();
+                }
+            };
         }
 
 }
--- app/src/main/java/com/babbangona/barcodescannerproject/SelectMSA.java
+++ app/src/main/java/com/babbangona/barcodescannerproject/SelectMSA.java
@@ -7,10 +7,15 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.SearchView;
 
 import com.babbangona.barcodescannerproject.model.msaT;
 import com.babbangona.bg_face.LuxandActivity;
@@ -273,12 +278,58 @@ public class SelectMSA extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == 419) {
+        if (requestCode == 419 && data != null) {
             if (data.getIntExtra("RESULT", 0) == 1) {
                 Message.message(getApplicationContext(), "Face Authenticated");
             } else {
                 Message.message(getApplicationContext(), "Face Not Authenticated");
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search)
+                .getActionView();
+        searchView.setSearchableInfo(searchManager
+                .getSearchableInfo(getComponentName()));
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        // listening to search query text change
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // filter recycler view when query submitted
+                msaAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // filter recycler view when text is changed
+                msaAdapter.getFilter().filter(query);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
